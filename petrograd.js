a667d78e7d90a9f49506af22c844d54cb626e20a
karo0353 <karo0353@stud.kea.dk>
2017-02-23 11:16:15 +0100

    add javascript template produkt

--- petrograd.js
+++ petrograd.js
@@ -1,41 +1,18 @@
-//$(document).on("load", start);
-window.addEventListener("load", start);
+window.addEventListener("load", sidenVises);
 
 
 
-function start() {
-    console.log("Start programmet");
-
-    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visPersonListe);
+function sidenVises() {
+    console.log("siden vises");
+    visProdukt();
 
 }
 
-function visPersonListe(listen) {
-    console.log(listen);
+function visProdukt() {
+    // klon produkt_template
+    var klon = document.querySelector("#produkt_template").content.cloneNode(true);
+    // indsæt data i klon
 
-    listen.forEach(visPersonInfo);
-
-}
-
-function visPersonInfo(person)  {
-    // klon person-info templaten
-
-    var klon = document.querySelector("#menuinfo_template").content.cloneNode(true);
-
-    // sæt data ind i klonen
-    klon.querySelector(".data_forret").innerHTML = person.forret;
-    klon.querySelector(".data_forret2").innerHTML = person.forret;
-
-
-    klon.querySelector(".modal").id = "modal" + person.id;
-    klon.querySelector(".modalknap").dataset.target = "#modal" + person.id;
-
-    console.log("Vis info");
-    console.log("fornavn: " + person.fornavn);
-    console.log("efternavn: " + person.efternavn);
-
-
-
-    // sæt klonen ind i person-info div
-    document.querySelector("#menu_info").appendChild(klon);
+    // append klon til .produkt_liste
+    document.querySelector(".produktliste").appendChild(klon);
 }
